51560c82ab94bbc1459971840acf0273f0cecaa8
Sylvia <79654653+status-code-js@users.noreply.github.com>
2021-07-13 00:57:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event) {                             //здесь лучше не обращаться к this, а event.target (это и есть элемент, который мы перетаскиваем)         
+event.target.classList.add('hold')                  //выделяется  цветом
+setTimeout(() => event.target.classList.add('hide'), 0)
+
+}
+
+function dragend(event) {
+event.target.className = 'item'
+}
+
+function dragover(event) {
+    event.preventDefault()
+    }
+
+function dragenter(event) {
+    event.target.classList.add('hovered')               //наведенный
+}
+
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
